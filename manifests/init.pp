cb065083f84f9e290c7c303df24261640bfeda66
Dustin Morgan <dustin_morgan@cable.comcast.com>
2015-09-18 18:06:31 -0600

    update 0.1.4

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,22 +2,15 @@
 #
 # This module manages artifact
 #
-# Parameters: $source, $target, $update, $rename, $purge,
-#             $swap, $owner, $group, $mode
+# Parameters: $source, $target, $update, $rename, $purge, $swap, $owner, $group, $mode
 #
 # Actions:
 #
-# Requires: curl
+# Requires: see Modulefile
 #
 # Sample Usage:
-# artifact { 'artifact.war':
-#  source => 'http://example.com/pub/artifact.war',
-#  target => '/home/tomcat/webapps',
-#  update => true,
-#  rename => 'current.war',
-#}
-define artifact ($source, $target, $update = false, $rename = undef,
-  $purge = false, $swap = '/tmp',) {
+#
+define artifact ($source, $target, $update = false, $rename = undef, $purge = false, $swap = '/tmp', $timeout = 0) {
   validate_bool($update)
   validate_bool($purge)
   validate_absolute_path($target)
@@ -25,14 +18,10 @@ define artifact ($source, $target, $update = false, $rename = undef,
   validate_string($source)
   validate_string($title)
 
-  # Just incase someone removes curl or diff
-  package { ['curl', 'diffutils']: ensure => 'installed' }
-
   # Some overly scoped path to help functionality across platforms
-  $path = '/bin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/share/bin:~/bin'
+  $path = '/bin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/share/bin:/usr/share/bin'
   $resource = $title
 
-  # Do we rename
   if ($rename != undef) {
     validate_string($rename)
     $full_target = "${target}/${rename}"
@@ -40,36 +29,40 @@ define artifact ($source, $target, $update = false, $rename = undef,
     $full_target = "${target}/${resource}"
   }
 
-  # Here's the magic, I would have used md5sum except there wasn't an elegant
-  # way to use it because the puppet shell provider is not
-  # bash and it causes a syntax exception in sh, hence TODO
-  # [TODO] Request puppetlabs add the ability to specify shell provider
-
   if ($update == false) {
     if ($purge == true) {
-      exec { "purging then fetching artifact ${resource}":
+      exec { "purging and fetching artifact ${resource}":
         path     => $path,
         provider => 'shell',
-        command  => "rm -f ${full_target} && curl -sko ${full_target} ${source}",
+        command  => "rm -f ${full_target} && curl -so ${full_target} ${source}",
         creates  => $full_target,
-        require  => [Package['curl'], Package['diffutils']],
+        timeout  => $timeout
       }
     } else {
       exec { "fetching artifact ${resource}":
         path     => $path,
         provider => 'shell',
-        command  => "curl -sko ${full_target} ${source}",
+        command  => "curl -so ${full_target} ${source}",
         creates  => $full_target,
-        require  => [Package['curl'], Package['diffutils']],
+        timeout  => $timeout
       }
     }
-  } else {
+  } elsif ($rename == undef) {
     exec { "updating ${resource}":
       path     => $path,
       provider => 'shell',
       command  => "mv -f /tmp/${resource} ${full_target}",
-      onlyif   => "touch ${full_target} && curl -sko ${swap}/${resource} ${source} && diff ${swap}/${resource} ${full_target}|grep differ",
-      require  => [Package['curl'], Package['diffutils']],
+      onlyif   => "touch ${full_target} && curl -so ${swap}/${resource} ${source} && diff ${swap}/${resource} ${full_target}|grep differ",
+      timeout  => $timeout
+    }
+  } elsif ($rename != undef) {
+    exec { "updating ${resource}":
+      path     => $path,
+      provider => 'shell',
+      command  => "mv -f /tmp/${rename} ${full_target}",
+      onlyif   => "touch ${full_target} && curl -so ${swap}/${rename} ${source} && diff ${swap}/${rename} ${full_target}|grep differ",
+      timeout  => $timeout
     }
   }
 }
+
